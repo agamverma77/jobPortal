3c625c670981c5c8fcafd9174a9cb9ef72014c57
ashu122112 <ashutosh2027@gmail.com>
2025-06-09 10:42:29 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MantineProvider, Slider } from '@mantine/core';
+import { createTheme, MantineProvider, Slider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import HomePage from './Pages/HomePage';
 
 function App() {
+  const theme = createTheme({
+    colors:{
+      'bright-sun': ['#fffbeb','#fff3c6','#ffe588','#ffd149','#ffbd20','#f99b07','#dd7302','#b75006','#943c0c','#7a330d','#461902',
+      ],
+      'mine-shaft': ['#f6f6f6','#e7e7e7','#d1d1d1','#b0b0b0','#888888','#6d6d6d','#5d5d5d','#4f4f4f','#454545','#3d3d3d','#2d2d2d',
+      ],
+    }
+  })
   return (
-    <MantineProvider>
+    <MantineProvider >
       <HomePage/>
     </MantineProvider>
+    
   );
 }
 
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,27 +1,31 @@
-import { Avatar } from '@mantine/core';
-import { IconAsset, IconBell, IconSettings } from '@tabler/icons-react';
+import { Avatar, Indicator } from '@mantine/core';
+import { IconAnchor, IconAsset, IconBell, IconSettings } from '@tabler/icons-react';
 
 const Header=()=>{
-    return <div className="w-full bg-mine-shaft-950 px-6 text-white h-28 flex justify-between items-center ">
-        <div className='flex gap-3 items-center'>
-            <IconAsset className='h-10 w-10' stroke={1.25}/>
-            <div className='text-3xl font-semibold'>iJobs</div>
+    return <div className="w-full bg-mine-shaft-950 px-6 text-white h-20 flex justify-between items-center ">
+        <div className='flex gap-1 items-center text-bright-sun-400'>
+            <IconAnchor className='h-10 w-8' stroke={2.5}/>
+            <div className='text-3xl font-semibold'>JobHook</div>
         </div>
-        <div className='flex gap-3'>
+        <div className='flex gap-5'>
             <a href="">Find Jobs</a>
             <a href="">Find Talent</a>
             <a href="">Upload Job</a>
             <a href="">About Us</a>
         </div>
-        <div className='flex gap-5 items-center'>
-            <IconBell/>
+        <div className='flex gap-3 items-center'>
+            
             <div className='flex items-center gap-3'>
                 <div>Marshal</div>
-                <Avatar src="avatar.png" alt="it's me" />
+                <Avatar src="avatar-9.png" alt="it's me" />
             </div>
-            <IconSettings/>
-            <div>
-                "gjh"
+            <div className='bg-mine-shaft-900 p-1.5 rounded-full'>
+                <IconSettings stroke={1.5}/>
+            </div>
+            <div className='bg-mine-shaft-900 p-1.5 rounded-full'>
+                <Indicator color="pink" offset={6} size={8} processing>
+                    <IconBell/>
+                </Indicator>
             </div>
         </div>
     </div>
